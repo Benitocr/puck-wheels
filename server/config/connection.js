0137ba588cd217e8e78c9a9f08e4d39dbe90853e
samini1 <92944248+samini1@users.noreply.github.com>
2022-04-12 20:55:03 -0500

    connect to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/puck-wheels');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/still-peak-39563');
 
 module.exports = mongoose.connection;
